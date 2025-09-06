9f758e6519fbb24c19ca1b5987d9f4cb05619881
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-06 22:09:52 +0530

    Initial commit

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        //My first java program
+        /*
+        Learning
+        Comments
+        Multi-line
+         */
+        System.out.println("Hello World\n");
+        System.out.println("I am Rachana");
+        System.out.print("");
+
+    }
+}
