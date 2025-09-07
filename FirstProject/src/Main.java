ab6962e3b8eaf51c0ad78b23bc670a5ebfd3a76e
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-07 19:36:50 +0530

    User inputs

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         //My first java program
@@ -20,6 +22,7 @@ public class Main {
 
 //        Steps to create variable: 1) Declaration 2) Assignment
 
+        //Primitive data types:
         int age; //declaration
         age=21; //assignment or initialization
         int year=2025;
@@ -35,6 +38,48 @@ public class Main {
 
         System.out.println("Rs "+price);
 
+        char grade='A';
+        char symbol='!';
+        char currency='$';
+        System.out.println(grade);
+        System.out.println(symbol);
+        System.out.println(currency);
+        System.out.println(""+grade+symbol+currency); //use "" otherwise will give output 134
 
+//        boolean isStudent = true; //camel case naming convention
+        boolean forSale=false;
+        boolean isOnline=true;
+
+//        System.out.println(isStudent+forSale++isOnline); will give error : bad operand types for binary operator +
+//        System.out.println(isStudent+" "+forSale+" "+isOnline); //seen in if statements
+
+        //Reference data types:
+        // string - series of characters
+        String name = "Rachana R Aithal";
+        System.out.println("Hello "+name);
+        System.out.println("You are "+ age+" years old");
+        System.out.println("Your gpa is "+ gpa);
+        System.out.println("Your grade is "+grade);
+        System.out.println("The car costs "+currency+price);
+
+        Scanner scanner = new Scanner(System.in); //scanner object can read user input from standard input
+        System.out.print("Enter your name:");
+        name=scanner.nextLine();
+        System.out.println("Hello"+name);
+        System.out.println("Enter college");
+        String college=scanner.next(); // Reads single word
+
+        System.out.println("Are you a student? (true/false)");
+        boolean isStudent=scanner.nextBoolean();
+        //nextInt(), nextDouble(), nextBoolean()
+        scanner.close(); //good practice to close at end else unexpected behavior.
+        System.out.println("your college is "+college);
+
+        if(isStudent){
+            System.out.println("You are a student!");
+        }
+        else{
+            System.out.println("You are not a student!");
+        }
     }
 }
