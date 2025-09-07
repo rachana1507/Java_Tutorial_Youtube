01754780176cc0c5c907c0ce18f91ea3ff196384
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-07 14:35:26 +0530

    Merge remote main into local main

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -10,5 +10,31 @@ public class Main {
         System.out.println("I am Rachana");
         System.out.print("");
 
+//        Variable is a reusable container for values.
+
+//        Primitive variables: A simple value stored directly in memory (stack).
+//        Eg:- int, double, char, boolean
+
+//        Reference variables: Memory address(in a stack) that points to the (location in heap)
+//        Eg:- string, array, object
+
+//        Steps to create variable: 1) Declaration 2) Assignment
+
+        int age; //declaration
+        age=21; //assignment or initialization
+        int year=2025;
+        int quantity=1;
+        System.out.print(age);
+        System.out.print(" ");
+        System.out.println(year+" "+quantity);
+        System.out.println("The year is "+year);
+
+        double price=12;
+        double gpa =8.27;
+        double temperature= -12.5;
+
+        System.out.println("Rs "+price);
+
+
     }
 }
