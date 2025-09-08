5230f0480eff070823506eedf153f5802acd1ddd
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-08 19:07:55 +0530

    if statements

--- CompoundInterestCalculator/src/Main.java
+++ CompoundInterestCalculator/src/Main.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+//        Scanner scanner=new Scanner(System.in);
+//        //if statements
+//        int age;
+//        System.out.print("Enter your age: ");
+//        age=scanner.nextInt();
+//        if(age>=65){
+//            System.out.println("You are a senior citizen");
+//        }
+//        else if(age>=18){
+//            System.out.println("You are an adult");
+//        }
+//        else if(age<0){
+//            System.out.println("You havent been born yet");
+//        }
+//        else{
+//            System.out.println("You are a child");
+//        }
+//        scanner.close();
+
+        
+    }
 }
