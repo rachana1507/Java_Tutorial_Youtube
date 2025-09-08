52a755f31da2affdc40898514ef5dd9c2efdcbc9
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-08 20:36:27 +0530

    random numbers

--- CompoundInterestCalculator/src/Main.java
+++ CompoundInterestCalculator/src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args){
-//        Scanner scanner=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
 //        //if statements
 //        int age;
 //        System.out.print("Enter your age: ");
@@ -19,8 +20,29 @@ public class Main {
 //        else{
 //            System.out.println("You are a child");
 //        }
-//        scanner.close();
 
-        
+        //*******************************************
+
+        Random random= new Random();
+        int number1;
+        int number2;
+        int number3;
+
+//        number=random.nextInt();// very large number
+        number1=random.nextInt(1,7);
+        System.out.println(number1);
+
+        number2=random.nextInt(1,7);
+        System.out.println(number2);
+
+        number3=random.nextInt(1,7);
+        System.out.println(number3);
+//        random.nextDouble() generates random number between 0 and 1 (decimal number)
+//        random.nextBoolean() can be used for situations like flipping a coin
+
+        scanner.close();
+
+
+
     }
 }
