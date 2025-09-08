db6a3ba258db02860b186eae9cc56c185bae7849
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-08 15:07:59 +0530

    operators

--- ShoppingCartProgram/src/Main.java
+++ ShoppingCartProgram/src/Main.java
@@ -1,2 +1,37 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Arithmetic Operators
+//        int x=10;
+//        int y=2;
+//        int z;
+
+//        z=x+y;
+//        z=x-y;
+//        z=x*y;
+//        z=x/y;
+//        z=x%y;
+//        int a=6;
+//        int b=4;
+//        z=b%a;
+//        z=a%b;
+//        System.out.println(z);
+
+//        // Augmented Assignment Operators
+//        int x=10;
+//        int y=3;
+////        x=x+y; // There is a shortcut for this
+//        x += y;
+//        x -=y;
+//        x*=y;
+//        x/=y;
+//        System.out.println(x);
+
+
+
+
+
+        scanner.close();
+    }
 }
