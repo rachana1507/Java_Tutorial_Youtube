993261c5cb94d64401875f95f30210f3120d3a6a
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-14 17:24:40 +0530

    string methods

--- 6_nestedif_string_substring/src/Main.java
+++ 6_nestedif_string_substring/src/Main.java
@@ -1,32 +1,59 @@
 public class Main {
     public static void main(String[] args){
-        boolean isStudent=true;
-        boolean isSenior=true;
+        //Nested ifs
+//        boolean isStudent=true;
+//        boolean isSenior=true;
+//        double price=9.99;
+//        if(isStudent){
+//            if(isSenior){
+//                System.out.println("You get a senior discount of 20%");
+//                System.out.println("You get a student discount of 10%");
+//                price*=0.7;
+//            }
+//            else{
+//                System.out.println("You get a student discount of 10%");
+//                price*=0.9;
+//            }
+//        }
+//        else{
+//            //price remains same
+//            if(isSenior){
+//                System.out.println("You get a senior discount of 20%");
+//                price*=0.8;
+//            }
+//            else{
+//                price*=1;
+//            }
+//        }
+//        System.out.printf("The price of a ticket is: $%.2f",price);
 
-        double price=9.99;
+        //Strings
+        String name="Rachana Aithal";
+        int length=name.length();
+        char letter=name.charAt(8);
+        int index=name.indexOf("a",2);
+        int lastindex=name.lastIndexOf("a");
 
-        if(isStudent){
-            if(isSenior){
-                System.out.println("You get a senior discount of 20%");
-                System.out.println("You get a student discount of 10%");
-                price*=0.7;
-            }
-            else{
-                System.out.println("You get a student discount of 10%");
-                price*=0.9;
-            }
-        }
-        else{
-            //price remains same
-            if(isSenior){
-                System.out.println("You get a senior discount of 20%");
-                price*=0.8;
-            }
-            else{
-                price*=1;
-            }
-        }
+        System.out.println(name+length+" "+letter+index);
+        System.out.println(name);
+        System.out.println(lastindex);
 
-        System.out.printf("The price of a ticket is: $%.2f",price);
+        name=name.toUpperCase();
+        System.out.println(name);
+        name=name.toLowerCase();
+        System.out.println(name);
+
+        String trimmingstr="     hei na    ";
+        System.out.println(trimmingstr);
+        trimmingstr=trimmingstr.trim();
+        System.out.println(trimmingstr);
+
+        trimmingstr=trimmingstr.replace("i","y");
+        System.out.println(trimmingstr);
+
+        String check=" ";
+        System.out.println(check.isEmpty());
+
+        
     }
 }
