0871ba70abb1d3ca614dc3b25b2de0e3b676de09
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-15 18:04:32 +0530

    temperature converter

--- 8_TemperatureConverter/src/Main.java
+++ 8_TemperatureConverter/src/Main.java
@@ -1,23 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         // Ternary operator ? -Return 1 of 2 values if a condition is true
         // variable= (condition) ? ifTrue : ifFalse;
-        int score=70;
-        if( score>=60){
-            System.out.println("Pass");
-        }
-        else{
-            System.out.println("Fail");
-        }
+//        int score=70;
+//        if( score>=60){
+//            System.out.println("Pass");
+//        }
+//        else{
+//            System.out.println("Fail");
+//        }
+//
+//        String result= (score>=60) ? "Pass": "Fail";
+//        System.out.println(result);
+//
+//        int hours=12;
+//        int time=(hours>12)? hours-12: hours;
+//        String tod=(hours<12)? "am":"pm";
+//        System.out.println(time+tod);
 
-        String result= (score>=60) ? "Pass": "Fail";
-        System.out.println(result);
+        //Temperature converter
 
-        int hours=12;
-        int time=(hours>12)? hours-12: hours;
-        String tod=(hours<12)? "am":"pm";
-        System.out.println(time+tod);
+        Scanner scanner=new Scanner(System.in);
 
-        
+        double temp;
+        double newtemp;
+        String unit;
+
+        System.out.print("Enter the temperature: ");
+        temp=scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
+        unit=scanner.next().toUpperCase(); //method chaining
+
+        newtemp=(unit.equals("C"))? (temp-32)*5/9 : (9*temp)/5 +32;
+
+        System.out.printf("The new temperature is %.2f degrees",newtemp);
+
+
+        scanner.close();
     }
 }
