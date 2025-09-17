ec5a79e47379b191be2b5d2dacad26d9489847f0
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-17 11:23:50 +0530

    for loop countdown

---
+++ 12_ForLoops/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        // FOR LOOP - Executes some code a CERTAIN amount of times
+
+//        for(int i=0 ;i<10 ;i++ ){
+//            System.out.println(i);
+//        }
+
+        // COUNTDOWN
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("How many seconds to countdown from?: ");
+        int start=scanner.nextInt();
+
+        for(int i=start;i>0;i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("HAPPY NEW YEAR!!!");
+
+        scanner.close();
+    }
+}
