050375464ce7e90d4cce9e7007fae6e982b58378
Rachana-R-Aithal <rachana.aithal24@gmail.com>
2025-09-23 10:17:54 +0530

    Quiz game

---
+++ 18_SimpleArrayGames/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        // Quiz game
+
+        // array of questions
+        String[] questions={"What is the main function of a router?",
+                "Which part of the computer is considered the brain?",
+                "What year was facebook launched?",
+                "Who is known as the father of computer?",
+                "What was the first programming language?"};
+
+        // options - 2d array
+        String[][] options={{"1. Storing files", "2. Encrypting data","3. Directing internet traffic", "4. Managing passwords"},
+                {"1. CPU","2. Hard drive","3. RAM","4. GPU"},
+                {"1. 2000","2. 2004","3. 2006","4. 2008"},
+                {"1. Steve Jobs","2. Bill Gates","3. Alan Turing","4. Charles Babbage"},
+                {"1. COBOL","2. C","3. Fortran","4. Assembly"}};
+
+        // declare variables
+        int[] answers={3, 1, 2, 4, 3};
+        int score=0;
+        int guess;
+        Scanner scanner=new Scanner(System.in);
+
+        // welcome message
+        System.out.println("******************************");
+        System.out.println("Welcome to the Quiz game!");
+        System.out.println("******************************");
+
+        // list question - loop
+        for(int i=0; i<questions.length;i++){
+            System.out.println(questions[i]);
+            //   options
+            for(String option: options[i]){
+                System.out.println(option);
+            }
+            //   get guess from user
+            System.out.print("Your Choice is: ");
+            guess=scanner.nextInt();
+            //   check our guess
+            if(guess==answers[i]){
+                score+=1;
+            }
+            System.out.println("Correct Answer: "+answers[i]);
+            System.out.println("******************************");
+        }
+        // display final score
+        System.out.println("******************************");
+        System.out.println("Your score is: "+score);
+        System.out.println("******************************");
+
+
+
+
+
+        scanner.close();
+    }
+}
